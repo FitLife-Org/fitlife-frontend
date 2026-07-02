9ff1d719de92d354ce02bb39496f86acef9032ce
Nguyen Tuan Khoa <nguyenkhanh7510@gmail.com>
2026-07-02 21:10:30 +0700

    feat(routing): remove auto redirect on home page to allow logged in users to view landing page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,7 @@
-import { Navigate } from "react-router-dom";
-import { useAuthStore } from "../store/authStore";
 import GuestLayout from "../components/layout/GuestLayout";
 import LandingPage from "./guest/LandingPage";
-import { getRedirectPathByRoles } from "../utils/authRedirect";
 
 export default function HomePage() {
-  const { isAuthenticated, user } = useAuthStore();
-  
-  if (isAuthenticated && user) {
-     return <Navigate to={getRedirectPathByRoles(user.roles)} replace />;
-  }
-  
   return (
     <GuestLayout>
       <LandingPage />
